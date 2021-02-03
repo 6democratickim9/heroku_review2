4a19fa6e8bc9e49ec9b14d072cdccba60061ad31
6democratickim9 <shcomon24@gmail.com>
2021-02-03 18:14:21 +0900

    수정

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/',(req,res) => res.send('WELCOME!'));
 //     res.send('LUULUULUULLUULUUL[' + user_name + ']');
 // });
 app.get('/service',(req,res) =>  {
-    var status = req.param('on')
-    res.send('Response message: The system is running [Status:ON]');
+    var run = req.param('status')
+    res.send('Response message: The system is running ['+run+']');
 });
 app.get('/check',(req,res) =>  res.send("NOMNOMNOMONOMONOMNOMONONO"));
 app.listen(port,() => console.log('Example app listening on port 3000'))
