d30db71d69325a5cb83d5d049303281566555f35
6democratickim9 <shcomon24@gmail.com>
2021-02-03 17:30:40 +0900

    ss

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000
 // http://127.0.0.1:3000/
 app.get('/',(req,res) => res.send('WELCOME!'));
-app.get('/welcome',(req,res) =>  {
-    var user_name = req.param('name')
-    res.send('LUULUULUULLUULUUL[' + user_name + ']');
-});
+// app.get('/welcome',(req,res) =>  {
+//     var user_name = req.param('name')
+//     res.send('LUULUULUULLUULUUL[' + user_name + ']');
+// });
 app.get('/service',(req,res) =>  {
     var on = req.param('status')
     res.send('Response message: The system is running [Status:ON]');
